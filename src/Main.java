ff4ac142e79e89af5a97b1ba843a1f77ef91f507
Harvendra Singh Rathore <harvendrasr1999@gmail.com>
2023-03-31 19:39:31 +0530

    Minor chnage in main function

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import SnakeAndLadderGame.Player;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, This is LLD Implementation Playlist");
     }
 }
